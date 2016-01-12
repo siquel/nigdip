c6abd81eb96cf330041df337dd9ea2012827ed19
siquel <squual@kahvipaussi.net>
2016-01-12 20:17:08 +0200

    Remove unnecessary test

--- bot.pl
+++ bot.pl
@@ -4,8 +4,3 @@ BEGIN {
 
 Nigdip::load('plugins/test.pl');
 Nigdip::unload('plugins/test.pl');
-if (open my $handle, '>plugins/test.pl') {
-	print $handle "print \"JEESUS SAATANA\"\n";
-	close $handle;
-}
-Nigdip::load('plugins/test.pl');
